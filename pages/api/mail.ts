23b7179ae3dcd5b379393bec1cb102b144d6b6a6
Ayodeji <drealayodeji@icloud.com>
2022-05-20 05:00:47 +0100

    Initial

---
+++ pages/api/mail.ts
@@ -0,0 +1,253 @@
+import { NextApiRequest, NextApiResponse } from "next";
+const nodemailer = require("nodemailer");
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      const { title, name, content, signature, people } = req.body;
+      //CREATE NODE MAILER TRANSPORTER
+      const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_SERVER,
+        port: process.env.EMAIL_PORT,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+
+      //EMAIL TEMPLATE
+      const Template = `<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.0 Transitional//EN” “https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd”>
+
+      <html
+        lang="en"
+        xmlns="http://www.w3.org/1999/xhtml"
+        xmlns:o="urn:schemas-microsoft-com:office:office"
+      >
+        <head>
+          <meta charset="UTF-8" /><meta http–equiv=“Content-Type” content=“text/html; charset=UTF-8” />
+            <meta http–equiv=“X-UA-Compatible” content=“IE=edge” />
+            <meta name=“viewport” content=“width=device-width, initial-scale=1.0 “ />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="x-apple-disable-message-reformatting" />
+          <meta name="color-scheme" content="only" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"
+            integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900"
+            rel="stylesheet"
+            type="text/css"
+          />
+          <title>${title}</title>
+      
+           <style>
+      :root {
+        --primary: #ff7605;
+        --secondary: #ffefe2;
+        --primary-alternate: #ac2c4b;
+        --secondary-alternate: #ffd3d0;
+      }
+      * {
+        -webkit-appearance: none !important;
+        -moz-appearance: none !important;
+        appearance: none !important;
+        max-width: 100% !important;
+        box-sizing: border-box !important;
+        font-family: Poppins, Helvetica, Arial, sans-serif !important;
+        margin: 0 !important;
+        padding: 0 !important;
+      }
+      html,
+      body {
+        width: 91% !important;
+        margin-inline: auto !important;
+        margin-top: 1rem !important;
+        background-color: rgb(250 250 250) !important;
+        color: rgb(35, 35, 35) !important;
+      }
+      header {
+        display: flex !important;
+        justify-content: center !important;
+        align-items: center !important;
+        background-color: #efefef !important;
+        padding-top: 1.4rem !important;
+        padding-bottom: 1.4rem !important;
+        margin-bottom: 1.25rem !important;
+      }
+      header img {
+        height: 1.75rem !important;
+      }
+      .HeaderText {
+        width: 85% !important;
+        font-size: 2.3rem !important;
+        padding-inline: 0.2rem !important;
+        font-weight: 600 !important;
+        line-height: 1.4 !important;
+      }
+      section.Content {
+        background-color: #efefef !important;
+        padding-inline: 1.4rem !important;
+        padding-top: 1.5rem !important;
+        padding-bottom: 1.5rem !important;
+        margin-top: 1.25rem !important;
+        margin-bottom: 1.4rem !important;
+        line-height: 1.75 !important;
+        font-weight: 400 !important;
+        font-size: 12px !important;
+      }
+      .Signature {
+        font-size: 0.85rem !important;
+        font-weight: 400 !important;
+        padding-inline: 0.2rem !important;
+      }
+      .Signature p {
+        font-weight: 500 !important;
+        margin-bottom: 0.4rem !important;
+      }
+      footer {
+        border-top: 1px solid #aaa !important;
+        text-align: center !important;
+        padding-top: 3.5rem !important;
+        padding-bottom: 6rem !important;
+        margin-top: 4rem !important;
+        font-weight: 300 !important;
+      }
+      footer .Links {
+        padding-top: 2.5rem !important;
+        padding-bottom: 2.5rem !important;
+        width: 100% !important;
+        margin-right: auto !important;
+        margin-left: auto !important;
+      }
+      footer h4 {
+        font-size: 0.75rem !important;
+      }
+      footer h5 {
+        font-size: 0.7rem !important;
+      }
+      footer .Copyright {
+        font-size: 0.6rem !important;
+        padding-top: 2rem !important;
+      }
+      .LinkImage {
+          width: 0.9rem !important;
+          aspect-ratio: 1 / 1 !important;
+          object-fit: contain !important;
+      }
+      .code {
+        margin-top: 1.2rem !important;
+        margin-bottom: 1.2rem !important;
+        letter-spacing: 0.4rem !important;
+      }
+      .footerText {
+        font-size: 8px !important;
+      }
+      @media (min-width: 768px) {
+        body,
+        html {
+          width: 60vw !important;
+          margin-right: auto !important;
+          margin-left: auto !important;
+        }
+        footer .Links {
+          padding-top: 3rem !important;
+          padding-bottom: 3rem !important;
+        }
+      }
+    </style>
+        </head>
+      
+        <body>
+          <main class="main">
+            <header>
+              <img
+                src="https://res.cloudinary.com/breege-tech/image/upload/v1653004497/Logo_bewtro.svg"
+                alt="Logo"
+              />
+            </header>
+      
+            <div class="content justify-center text-neutral-700 mt-4 text-sm">
+              <h4 class="HeaderText">
+                <!-- title -->
+                ${title}
+              </h4>
+      
+              <section class="Content">
+                <!-- CONTENT -->
+                ${content}
+              </section>
+      
+              <div class="Signature">
+                <p>Regards,</p>
+                ${signature}
+              </div>
+            </div>
+      
+            <footer>
+              <h4>The all-in-one social media for Techies.</h4>
+              <div class="Links flex justify-evenly">
+                <a href="https://twitter.com/frikax">
+                <img src="https://res.cloudinary.com/breege-tech/image/upload/v1653018283/Icons/Twitter_nnz5wk.svg" alt="Twitter_icon" class="LinkImage" />
+                </a>
+                <!-- <a href="https://instagram.com/_frikax"
+                  ><img src="https://res.cloudinary.com/breege-tech/image/upload/v1653018283/Icons/Instagram_hobvfx.svg" alt="Instagram_icon" class="LinkImage" /></a> -->
+                <a href="https://facebook.com/frikax"
+                  ><img src="https://res.cloudinary.com/breege-tech/image/upload/v1653018283/Icons/Facebook_kz6zrl.svg" alt="FB_icon" class="LinkImage" /></a>
+              
+              <a href="https://www.linkedin.com/company/frikax"
+                  ><img src="https://res.cloudinary.com/breege-tech/image/upload/v1653018283/Icons/LinkedIn_niran3.svg" alt="LinkedIn_icon" class="LinkImage" /></a>
+              </div>
+      
+              <h5>
+                Join us at
+                <a href="https://www.frikax.net" class="underline">Frikax</a>
+                or reach out to us on social media.
+              </h5>
+              <div class="Copyright">&copy; Frikax 2022, All rights reserved.</div>
+            </footer>
+          </main>
+        </body>
+      </html>
+      `;
+
+      //Loops through data
+      people.forEach(async (person: any) => {
+        const mail = await transporter.sendMail({
+          from: '"' + name + '" <ask@frikax.net>',
+          to: `${person.email}`,
+          subject: title,
+          text: "Sorry you can not view this message on your current device!",
+          html: Template,
+        });
+
+        if (!mail) {
+          throw { status: 500, message: "Something went wrong" };
+        }
+      });
+
+      return res
+        .status(200)
+        .json({ status: "success", message: "All mails have been sent!" });
+    } catch (err: any) {
+      console.error(err);
+      return res
+        .status(err?.status || 500)
+        .json({ status: "error", message: "Something went wrong" });
+    }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,151 @@
+import { FC, useState } from "react";
+import dynamic from "next/dynamic";
+import type { NextPage } from "next";
 import Dummy from "@/components/Dummy";
 import Meta from "@/defaults/Meta";
-import type { NextPage } from "next";
+import axios from "axios";
+import { BsX } from "react-icons/bs";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import API_URL from "../config/api";
+const SunEditor = dynamic(() => import("suneditor-react"), {
+  ssr: false,
+});
+import "suneditor/dist/css/suneditor.min.css";
+const file = require("../data/data.json");
 
-const Home: NextPage = () => {
+interface DataInterface {
+  title: string;
+  signature: string;
+  name: string;
+}
+
+interface EmailInterface {
+  email: string | any;
+}
+
+const Home: NextPage<null> = () => {
+  //DATA STATE
+  const [Content, setContent] = useState<string>("");
+  const [Emails, setEmails] = useState<EmailInterface[]>(file);
+  const [Data, setData] = useState<DataInterface>({
+    title: "",
+    signature: "Frokes, Co-founder",
+    name: "Frokes From Frikax",
+  });
+  const [Loading, setLoading] = useState<boolean>(false);
+
+  //Remove Email From List
+  const RemoveData = (data: string): void => {
+    const newData: EmailInterface[] = Emails.filter(
+      (current) => current.email.toLowerCase() !== data.toLowerCase()
+    );
+    setEmails(newData);
+  };
+
+  //Handle Submission
+  const formHandler = () => {
+    if (confirm("Are you sure you want to send this email?")) {
+      if (!(Data.title && Data.signature && Data.name && Content)) {
+        toast.error("All fields are required!");
+      } else {
+        setLoading(true);
+        axios
+          .post(`${API_URL}/mail`, {
+            title: Data.title.trim(),
+            content: Content,
+            signature: Data.signature,
+            name: Data.name,
+            people: Emails,
+          })
+          .then((response) => {
+            setLoading(false);
+            toast.success(response.data.message);
+          })
+          .catch((err) => {
+            setLoading(false);
+            toast.error(err.message);
+          });
+      }
+    }
+  };
+
   return (
     <div>
-      <Meta title="Home" desc="This is a description" />
-      Hello World!
-      <Dummy />
+      <Meta title="Mass Email Sender" desc="For sending mass emails" />
+      <main className="w-[95%] lg:w-4/5 2xl:w-4/6 mx-auto mt-[2vh] lg:mt-[5vh]">
+        {Loading && (
+          <div className="Loader absolute top-6 right-6 w-8 aspect-square bg-transparent border-t-[3px] border-l-[3px] border-[3px] border-t-neutral-100 border-l-neutral-100 border-neutral-700 rounded-full"></div>
+        )}
+        <h1 className="font-thin text-4xl lg:text-6xl">Compose Email</h1>
+        <section className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-8 mt-10">
+          <div>
+            <input
+              name="Title"
+              placeholder="Title"
+              className="w-full bg-neutral-800 text-neutral-300 focus:outline-none focus:border-neutral-500 focus:border-[1px] placeholder-neutral-500 py-4 px-3 mb-4"
+              value={Data.title}
+              onChange={(e) => setData({ ...Data, title: e.target.value })}
+            />
+            <SunEditor
+              height="40vh"
+              setContents={Content}
+              onChange={(value) => setContent(value)}
+            />
+            <input
+              name="Name"
+              placeholder="Name ex. Frokes From Frikax"
+              className="w-full bg-neutral-800 text-neutral-300 focus:outline-none focus:border-neutral-500 focus:border-[1px] placeholder-neutral-500 py-4 px-3 my-4"
+              value={Data.name}
+              onChange={(e) => setData({ ...Data, name: e.target.value })}
+            />
+            <input
+              name="Signature"
+              placeholder="Signature ex. Frokes, Co-founder"
+              className="w-full bg-neutral-800 text-neutral-300 focus:outline-none focus:border-neutral-500 focus:border-[1px] placeholder-neutral-500 py-4 px-3 mb-4"
+              value={Data.signature}
+              onChange={(e) => setData({ ...Data, signature: e.target.value })}
+            />
+            <button
+              name="Send"
+              type="button"
+              className="w-full bg-neutral-300 text-neutral-900 text-sm hover:bg-neutral-700 hover:text-neutral-200 transition-all py-3"
+              onClick={formHandler}
+            >
+              Send
+            </button>
+          </div>
+          <div className="h-[75vh] max-h-[75vh] bg-neutral-800 px-3 lg:px-6 py-5">
+            <h1 className="font-thin text-3xl lg:text-4xl">Emails</h1>
+
+            <div className="mt-3 overflow-y-scroll">
+              {Emails.length > 0 ? (
+                <>
+                  {Emails.map((person, index) => (
+                    <div
+                      className="flex justify-between bg-neutral-700 text-neutral-300 my-3 lg:my-4 px-4 py-4"
+                      key={index}
+                    >
+                      <p>{person.email}</p>
+                      <button
+                        name="Remove"
+                        onClick={() => RemoveData(person.email)}
+                      >
+                        <BsX size={20} />
+                      </button>
+                    </div>
+                  ))}
+                </>
+              ) : (
+                <p className="text-center text-neutral-500 my-16">
+                  No data to display
+                </p>
+              )}
+            </div>
+          </div>
+        </section>
+      </main>
+      <ToastContainer />
     </div>
   );
 };
